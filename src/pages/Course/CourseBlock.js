d2e1a5b02bfe377756bfb2ae8aeaa230dd482e55
KristianMedeshi <kristianmedeshi@gmail.com>
2023-11-30 21:52:18 +0200

    add course page

--- src/pages/Course/CourseBlock.js
+++ src/pages/Course/CourseBlock.js
@@ -1,20 +1,123 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
-import { useQuery } from 'react-query';
+import React from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { useParams } from 'react-router-dom';
-import HTMLReactParser from 'html-react-parser';
-import { getCourseBlock } from '../../utils/coursesApi';
-import './Course.scss';
+import { Controller, useForm } from 'react-hook-form';
+import ReactQuill from 'react-quill';
+import { useTranslation } from 'react-i18next';
+import Field from '../../components/Field';
+import { getCourseBlock, markAsCompleted } from '../../utils/coursesApi';
+import Loader from '../../components/Loader';
 
 function CourseBlock({ id }) {
+  const [t] = useTranslation('global');
   const courseId = useParams().id;
-  const { data } = useQuery(`${courseId}/${id}`, () => getCourseBlock(courseId, id));
+  const { data, isLoading } = useQuery(`${courseId}/${id}`, () => getCourseBlock(courseId, id));
   const { block } = data || {};
 
+  const { register, control, handleSubmit } = useForm({ shouldFocusError: false });
+
+  const queryClient = useQueryClient();
+  const markCourseMutation = useMutation((data) => markAsCompleted(block._id, data), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(`courses/${courseId}`);
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    markCourseMutation.mutate(data);
+  };
+
+  if (isLoading) {
+    return <div className="flex w-full h-full items-center justify-center"><Loader /></div>;
+  }
+
   return (
-    <div>
-      {HTMLReactParser(block?.lecture ?? '')}
-    </div>
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <ReactQuill
+        value={block?.lecture}
+        readOnly
+        modules={{ toolbar: false }}
+      />
+      <div className="divider-x" hidden={!block?.tests.length} />
+      {block?.tests.map((test, index) => (
+        <div
+          key={test}
+          className="bg-secondary p-4 rounded-md"
+        >
+          <ReactQuill
+            value={test?.questionText}
+            readOnly
+            modules={{ toolbar: false }}
+          />
+          <Controller
+            name={`answersTests.${index}`}
+            control={control}
+            defaultValue=""
+            rules={{ required: t('emptyFieldError') }}
+            render={({ field }) => (
+              <div className="flex flex-col gap-1">
+                {test.options.map((option, indexInner) => (
+                  <label
+                    key={option}
+                    htmlFor={`${index}/${indexInner}`}
+                    className="flex gap-3 items-center hover:bg-primary
+                      px-3 py-3 rounded-md cursor-pointer"
+                  >
+                    <input
+                      id={`${index}/${indexInner}`}
+                      type="radio"
+                      {...field}
+                      value={option}
+                      className="test-radio"
+                    />
+                    {option}
+                  </label>
+                ))}
+              </div>
+            )}
+          />
+        </div>
+      ))}
+      <div className="divider-x" hidden={!block?.tasks.length} />
+      {block?.tasks.map((task, index) => (
+        <div
+          key={task}
+          className="bg-secondary p-4 rounded-md"
+        >
+          <ReactQuill
+            value={task?.title}
+            readOnly
+            modules={{ toolbar: false }}
+          />
+          <Field registerReturn={register(`answersTasks.${index}`)} />
+        </div>
+      ))}
+      <div className="divider-x" hidden={!block?.attachments.length} />
+      {block?.attachments.map((file) => (
+        <a
+          target="_black"
+          href={file.path}
+          rel="noreferrer"
+          className="bg-secondary border border-lines
+          px-2 py-1 rounded-sm whitespace-nowrap h-fit w-fit hover:underline"
+        >
+          {file.originalName}
+        </a>
+      ))}
+      <div className="divider-x" hidden={block.isPassed} />
+      <button
+        type="submit"
+        hidden={block.isPassed}
+        className="button-primary w-fit self-end px-5"
+        disabled={markCourseMutation.isLoading}
+      >
+        {t('courses.markAsCompleted')}
+      </button>
+    </form>
   );
 }
 
